09629c6f756e7ee0d801983795446aab97b4efe7
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-08-01 17:46:26 +0700

    fetch dummy char page

--- react-miniproject-d/src/pages/DetailPages/Characterpage.jsx
+++ react-miniproject-d/src/pages/DetailPages/Characterpage.jsx
@@ -6,19 +6,59 @@
 // useParams
 // } from "react-router-dom";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import TitleBackground from '../../components/DetailBackground/TitleBackground'
 import { FloatingLabel, Form , Badge, Button, Figure, Container ,Row, Col, Card} from 'react-bootstrap';
 import DetailNavBtn from '../../components/DetailNavButton.jsx/DetailNavBtn';
+import axios from 'axios';
 
 function Characterpage() {
+const number = Math.floor(Math.random() * 20)
+// console.log (number)
+
+const [charMale, setCharMale] = useState([])
+
+
+
+
+const getCharMale = async (url) => {
+try {
+const charMale = await axios.get(url);
+const data = await charMale.data;
+setCharMale(data.results)
+
+}catch (error) {
+console.log(error)
+}
+}
+useEffect(() => {
+getCharMale(`https://randomuser.me/api/?results=${number}&gender=male&inc=name,gender,picture`)
+}, [])
+console.log(charMale)
+// console.log (charMale.map((item, index)=>{
+// return
+// }))
+
+// const data1 = charMale.results
+// const nama = data1.name()
+// console.log (nama.fisrt)
 return (
 
 <>
     <TitleBackground />
     <Container>
-    <DetailNavBtn />
+        <DetailNavBtn />
+        <Container>
+            <ul>
+                {charMale.map((item, index)=>{
+                return <div key={index}>
 
+                    <h3>{item?.name?.first}</h3>
+
+                </div>
+                })}
+            </ul>
+        </Container>
     </Container>
 
     <div>
