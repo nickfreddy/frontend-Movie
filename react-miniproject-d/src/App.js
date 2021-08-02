90481003bf7a758114429a8da4f2c8b6b04dd0ec
Bagas-Syaffurqon-L20010174 <syaffurqonbagas@gmail.com>
2021-08-02 20:56:09 +0700

    New Admin page

--- react-miniproject-d/src/App.js
+++ react-miniproject-d/src/App.js
@@ -6,15 +6,17 @@ import Navbar_Admin from './components/header/Navbar_Admin';
 import Navbar_notSign from './components/header/Navbar_notSign';
 import Navbar_Sign from './components/header/Navbar_Sign';
 import Homepage from './pages/Homepage/Homepage';
+import AdminPage from './Pages Bagas/Admin-Page';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        {/* the routing components */}
-        <Routers />
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      {/* the routing components */}
+      {/* <Routers />
+      </BrowserRouter> */}
+      <AdminPage />
     </div>
   );
 }
