17a28bf75722a70ca04d15054d0054c3d51ab442
inokiardhi <ardhiinoki@gmail.com>
2021-08-03 23:03:01 +0800

    merge development to inoki

--- react-miniproject-d/src/routes/router.js
+++ react-miniproject-d/src/routes/router.js
@@ -1,19 +1,24 @@
 import { Switch, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage/Homepage";
-import Detailpage from "../Pages/DetailPages/DetailPage";
-import Characterpage from "../Pages/DetailPages/Characterpage";
-import Review from "../Pages/DetailPages/Reviewpage";
+import Detailpage from "../pages/DetailPages/DetailPage";
+import Characterpage from "../pages/DetailPages/Characterpage";
+import Review from "../pages/DetailPages/Reviewpage";
 import Footer from "../components/footer";
+<<<<<<< HEAD
 import Navbar_notSign from "../components/header/Navbar_notSign";
+=======
+import ProfilePage from "../Pages Bagas/Profile-page";
+>>>>>>> 38f68df54c4adca0ed2300892e9b8fdd7b82fb51
 
 
 const Routers = () => {
 
 
-  
+
   return (
     // switch -> just like switch case where it read routes/ browser url and return it accordingly
     <>
+<<<<<<< HEAD
     <Navbar_notSign/>
     <Switch>
       {/* exact means the path must be the same as the value  */}
@@ -42,6 +47,34 @@ const Routers = () => {
       </Route>
     </Switch>
     <Footer />
+=======
+      <Switch>
+        {/* exact means the path must be the same as the value  */}
+        {/* return this component if the router is root aka / */}
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="/DetailPage">
+          <Detailpage />
+        </Route>
+        <Route exact path="/DetailPage/Character/">
+          <Characterpage />
+        </Route>
+        <Route exact path="/DetailPage/Review/">
+          <Review />
+        </Route>
+        <Route exact path="/Pages Bagas/Profile-page">
+          <ProfilePage />
+        </Route>
+        {/*  * is for all routes that is not registered in here */}
+        <Route path="*">
+          <div>
+            <h1>PAGE NOT FOUND</h1>
+          </div>
+        </Route>
+      </Switch>
+      <Footer />
+>>>>>>> 38f68df54c4adca0ed2300892e9b8fdd7b82fb51
     </>
   );
 };
