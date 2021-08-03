98f93f97ee07ac69a5d16bdf9272eefac1d40987
Aulia Sutowijoyo <aulia.sutowijoyo@gmail.com>
2021-08-03 23:25:07 +0700

    redux

--- react-miniproject-d/src/App.js
+++ react-miniproject-d/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* the routing components */}
+              {/* the routing components */}
         <Routers />
       </BrowserRouter>
     </div>
--- react-miniproject-d/src/index.js
+++ react-miniproject-d/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "./store";
+import store from "./store";
+
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 
   , document.getElementById('root')
