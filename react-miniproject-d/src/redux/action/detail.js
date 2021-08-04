1f0f0b7033fd069183b41da4e8cce364a890d917
Bagas-Syaffurqon-L20010174 <syaffurqonbagas@gmail.com>
2021-08-04 21:23:51 +0700

    Add load-movie-redux

--- react-miniproject-d/src/redux/action/detail.js
+++ react-miniproject-d/src/redux/action/detail.js
@@ -1,38 +1,38 @@
 import {
     GET_DETAIL_BEGIN,
     GET_DETAIL_SUCCESS,
-    GET_DETAIL_FAIL, 
+    GET_DETAIL_FAIL,
     POST_REVIEW_BEGIN,
     POST_REVIEW_SUCCESS,
     POST_REVIEW_FAIL,
- }     from "../../store/actions/types";
- import axios from "axios";
+} from "../../store/actions/types";
+import axios from "axios";
 
- const BASE_URL = "https://api.themoviedb.org/3/movie/";
+const BASE_URL = "https://api.themoviedb.org/3/movie/";
 
- export const getDetail = (id) => async (dispatch) => {
-     dispatch({
-         type: GET_DETAIL_BEGIN,
-         loading: true,
-         error: null,
-     });
-     try {
-         const res = await axios.get(`${BASE_URL}+${id}+?api_key=4c2c9a58431c5b46e098bf4eed17c94b&language=en-US`);
-         dispatch({
-             type: GET_DETAIL_SUCCESS,
-             loading: false,
-             payload: res.data,
-             error: null,
-         });
-     }catch (err){
-         dispatch({
-             type: GET_DETAIL_FAIL,
-             error: err.response,
-         });
-     }
- };
- 
- export const postReview = (body) => async (dispatch) => {
+export const getDetail = (id) => async (dispatch) => {
+    dispatch({
+        type: GET_DETAIL_BEGIN,
+        loading: true,
+        error: null,
+    });
+    try {
+        const res = await axios.get(`${BASE_URL}+${id}+?api_key=4c2c9a58431c5b46e098bf4eed17c94b&language=en-US`);
+        dispatch({
+            type: GET_DETAIL_SUCCESS,
+            loading: false,
+            payload: res.data,
+            error: null,
+        });
+    } catch (err) {
+        dispatch({
+            type: GET_DETAIL_FAIL,
+            error: err.response,
+        });
+    }
+};
+
+export const postReview = (body) => async (dispatch) => {
     dispatch({
         type: POST_REVIEW_BEGIN,
         loading: true,
@@ -46,7 +46,7 @@ import {
             loading: false,
             error: null,
         });
-    }catch (err){
+    } catch (err) {
         dispatch({
             type: POST_REVIEW_FAIL,
             error: err.response,
---
+++ react-miniproject-d/src/redux/action/movie.js
@@ -0,0 +1,18 @@
+import { LOAD_ALL_MOVIE } from "./types"
+import axios from "axios";
+
+export const loadMovies = () => async (dispatch) => {
+    try {
+        const movies = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=ba4ce5d35b9081ae360eeb355f0acda9");
+        const dataResults = await movies.data;
+        const data = await dataResults.results;
+        console.log(data)
+        dispatch({
+            type: LOAD_ALL_MOVIE,
+            payload: data
+        })
+        // console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
