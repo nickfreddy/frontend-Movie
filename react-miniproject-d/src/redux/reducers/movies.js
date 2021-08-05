5bacf0a9438747e0014ac2ed46db1873c067fe93
Bagas-Syaffurqon-L20010174 <syaffurqonbagas@gmail.com>
2021-08-05 22:40:13 +0700

    Add Detail Redux

--- react-miniproject-d/src/redux/reducers/movies.js
+++ react-miniproject-d/src/redux/reducers/movies.js
@@ -1,20 +1,20 @@
-// import { LOAD_ALL_MOVIE } from "../action/types";
+import { LOAD_ALL_MOVIE } from "../action/types";
 
-// const initialState = {
-//     data: []
-// };
+const initialState = {
+    data: []
+};
 
-// const movies = (state = initialState, action) => {
-//     switch (action.type) {
-//         case LOAD_ALL_MOVIE:
-//             return {
-//                 ...state,
-//                 data: action.payload
-//             }
+const movies = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_ALL_MOVIE:
+            return {
+                ...state,
+                data: action.payload
+            }
 
-//         default:
-//             return state;
-//     }
-// }
+        default:
+            return state;
+    }
+}
 
-// export default movies
+export default movies
