dae438860f3cd27092d6de9545ae654beb9ad348
inokiardhi <ardhiinoki@gmail.com>
2021-08-07 12:06:22 +0800

    test

--- react-miniproject-d/src/pages/Homepage/Homepage.jsx
+++ react-miniproject-d/src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Carousel } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap'
 import axios from 'axios'
 import Card from '../../components/card/Card'
 import CategoryButton from '../../components/categoryButton/CategoryButton'
@@ -11,6 +11,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { loadMovies } from '../../redux/action/movie'
 import { searchMovieRdx } from '../../redux/action/search'
 import MyPagination from '../../components/pagination/MyPagination'
+import noResultFound from '../../components/noResultFound/noResultFound'
+
+
+
+
 
 
 
@@ -23,11 +28,11 @@ function Homepage() {
     const [errorMessage, setErrorMessage] = useState(null);
     const [loading, setLoading] = useState(true);
     const dispatch = useDispatch()
-    const USERID = localStorage.getItem('USERID');
+
 
     useEffect(() => {
         dispatch(loadMovies())
-
+        
     }, []);
 
     useEffect(() => {
@@ -62,8 +67,8 @@ function Homepage() {
         setLoading(true);
         setErrorMessage(null);
 
-        if (word === 'all') {
-            return setMovies(moviesData)
+        if ( word === 'all') {
+           return setMovies(moviesData)
         };
 
         axios.get(`https://demovie.gabatch13.my.id/movies/genres/${word}?page=1&limit=0`)
@@ -130,20 +135,19 @@ function Homepage() {
             <div className="container divider my-1 "></div>
 
             <div className="container d-flex flex-wrap justify-content-around my-1">
-                {movies.length > 0 ? movies.filter((movie, idx) => idx < 20).map(movie => (
+                {movies.length > 0 ?  movies.filter((movie, idx) => idx < 20).map( movie =>(
                     <div key={movie.idx}>
                         <Link className="text-decoration-none text-dark" to={`detailPage/${movie._id}`}>
                             <Card className="skala" title={movie.title} img={movie.poster} category={movie.genres.join(', ')} />
                         </Link>
-                    </div>
-                )) : <h3>not found</h3>}
+                    </div> 
+                    )) : <h1>asxa</h1> }
             </div>
+           
             {/* -------------end card------------- */}
 
             <div className="my-3">
                 <MyPagination onclick={handlePagination} />
-                {/* <Button href={`/Review-page/${USERID}`} ></Button> */}
-                <Button href={`/Profile-page/${USERID}`} ></Button>
             </div>
 
             {/*  ---------------------- */}
