7715db87e16b95390cbafa15fc4d617fc2ffcb0c
Bagas-Syaffurqon-L20010174 <syaffurqonbagas@gmail.com>
2021-08-08 16:02:39 +0700

    Fixing Delete Review

--- src/pages/DetailPages/Wishlist.jsx
+++ src/pages/DetailPages/Wishlist.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Wishlist() {
     return (
         <>
-        <div>
-            
-        </div>
+            <div>
+
+            </div>
         </>
     )
 }
